d98e75207278cec39facaf28fd2e6b7f49432966
crunchyintheory <crunchyintheory@users.noreply.github.com>
2017-07-17 16:38:34 -0500

    Add copy button to output

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let textArea = document.getElementsByTagName('textarea')[0];
 let pretty = document.getElementsByClassName('prettyprint')[0];
+let copybtn = document.getElementsByClassName('copy-icon')[0];
 const prettify = function() {
     pretty.className = 'prettyprint';
     pretty.innerText = textArea.value;
+    pretty.appendChild(copybtn);
     PR.prettyPrint();
 };
+new Clipboard('.copy-icon');
